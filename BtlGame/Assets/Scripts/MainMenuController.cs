e715a110ad8d932f79a147c72b7519ff82bd3c97
giangtx <36334907+giangtx@users.noreply.github.com>
2020-07-01 22:19:51 +0700

    submit
    thay đổi spriter
    add menu

---
+++ BtlGame/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Application.LoadLevel("Main");
+    }
+}
